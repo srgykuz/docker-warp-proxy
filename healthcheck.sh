8065324c55c9326c662339c06894ee48cb3480a6
Sergey Kuznetsov <srgykuz@gmail.com>
2026-04-28 13:36:08 +0300

    healthcheck.sh: add request timeout

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -s -f --socks5 127.0.0.1:$PROXY_PORT https://www.cloudflare.com/cdn-cgi/trace | grep -q warp=on
+curl -s -f -m 15 --socks5 127.0.0.1:$PROXY_PORT https://www.cloudflare.com/cdn-cgi/trace | grep -q warp=on
 code=$?
 
 if [ "$code" -ne 0 ] && [ -n "$RESTART" ] && [ "$RESTART" != "0" ]; then
