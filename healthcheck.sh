e92aef7cc33a8e4c65cfc11e2c9be3a68764b9e4
Sergey Kuznetsov <srgykuz@gmail.com>
2026-04-28 13:25:18 +0300

    healthcheck.sh: add support for automatic container restart

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+
 curl -s -f --socks5 127.0.0.1:$PROXY_PORT https://www.cloudflare.com/cdn-cgi/trace | grep -q warp=on
+code=$?
+
+if [ "$code" -ne 0 ] && [ -n "$RESTART" ] && [ "$RESTART" != "0" ]; then
+    kill 1
+fi
+
+exit $code
